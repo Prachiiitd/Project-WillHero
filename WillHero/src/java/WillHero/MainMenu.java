505ad44b5f5678da8c696345432c8fc4fff92b30
ShrJatin <jatin20563@iiitd.ac.in>
2021-12-04 03:22:22 +0530

    MainMenu in Progress: prompt for name added

--- WillHero/src/java/WillHero/MainMenu.java
+++ WillHero/src/java/WillHero/MainMenu.java
@@ -2,25 +2,35 @@ package WillHero;
 
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
+import javafx.css.PseudoClass;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.*;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Screen;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MainMenu implements Initializable {
@@ -45,6 +55,10 @@ public class MainMenu implements Initializable {
         Parent root = FXMLLoader.load(Objects.requireNonNull(WillHero.class.getResource("mainMenu.fxml")));
         Scene scene = new Scene(root);
 
+        Image icon = new Image(new FileInputStream(Objects.requireNonNull(getClass().getResource("mainIcon.png")).getPath()));
+        stage.setTitle("Will Hero");
+        stage.getIcons().add(icon);
+
         stage.setScene(scene);
         stage.show();
 
@@ -57,8 +71,10 @@ public class MainMenu implements Initializable {
     public void exit(Stage stage){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"You're about to Exit!", ButtonType.YES, ButtonType.NO);
         alert.setTitle("Exit");
+        alert.initStyle(StageStyle.UNDECORATED);
 
-        if (alert.showAndWait().get() == ButtonType.YES){
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() &&  result.get() == ButtonType.YES){
             stage.close();
         }
     }
@@ -85,9 +101,41 @@ public class MainMenu implements Initializable {
         scale.play();
     }
 
-    public void setNewGame(MouseEvent newGame){
+    public void setNewGame(MouseEvent newGame) throws FileNotFoundException {
         onClickZoom(this.newGame);
+
+        Image icon = new Image(new FileInputStream(Objects.requireNonNull(getClass().getResource("mainIcon.png")).getPath()));
+        ImageView imageView = new ImageView(icon);
+        imageView.setFitWidth(35);
+        imageView.setPreserveRatio(true);
+
+        TextInputDialog textInputDialog = new TextInputDialog();
+        textInputDialog.initStyle(StageStyle.UNDECORATED);
+        textInputDialog.setHeaderText(null);
+        textInputDialog.setContentText("Enter Your Name");
+        textInputDialog.setGraphic(imageView);
+
+        Alert alert = new Alert(Alert.AlertType.WARNING,"Name Can not be blank", ButtonType.OK);
+        alert.initStyle(StageStyle.UNDECORATED);
+        alert.setHeaderText(null);
+
+        Label label = new Label();
+
+        Optional<String> result = textInputDialog.showAndWait();
+        System.out.println((textInputDialog.getEditor().getText()));
+
+        result.ifPresent(name -> {
+            label.setText(name);
+
+            if (label.getText().length() == 0) {
+                alert.showAndWait();
+            } else {
+                System.out.println(label.toString());
+                System.out.println("load game page with label");
+            }
+        });
     }
+
 
     public void setResumeGame(MouseEvent resumeGame) {
         onClickZoom(this.resumeGame);
